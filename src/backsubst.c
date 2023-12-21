90a53ee7a10828e1283bc5d1e9e699eb71489f05
Sebastian Dechnik <sebastiandechnik@MacBook-Air-Sebastian-2.local>
2023-12-21 16:05:12 +0100

    poprawa mat->

--- src/backsubst.c
+++ src/backsubst.c
@@ -7,14 +7,23 @@
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 				/**
 				 * Tutaj należy umieścić właściwą implemntację.
-				 */
-	x->data[c]=b->data[c]/mat->data[r][c];
-	double temp;
-	for(int j=c-1;i>0;i--){
-	        for(int i=c;i>j;i--)
-        	temp+=x->data[i] * mat->data[i-1][i-1];
-	x->data[j]=(temp+b->data[j])/mat->data[j][j];
+				 */	
+for(int i=0 ; i < mat->r ; i++){
+		if( mat->data[i][0]==0)
+			return 1;
 	}
+	
+	if(mat->r != mat->c)
+		return 2;
+
+	x->data[mat->c]=b->data[mat->c]/mat->data[mat->r][mat->c];
+	double temp;
+	for(int j=(mat->c)-1;i>0;i--){
+	        for(int i=mat->c;i>j;i--)
+        	temp+=x->data[i][0] * mat->data[i-1][i-1];
+	x->data[j][0]=(temp+b->data[j][0])/mat->data[j][j];
+	}
+
 
 
 
