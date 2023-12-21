1ab6503ebaa069a69ddd369c01ad66e611c5baec
Sebastian Dechnik <sebastiandechnik@MacBook-Air-Sebastian-2.local>
2023-12-21 15:24:43 +0100

    Wsteczne podstawienie

--- src/backsubst.c
+++ src/backsubst.c
@@ -8,6 +8,16 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 				/**
 				 * Tutaj należy umieścić właściwą implemntację.
 				 */
+	x->data[c]=b->data[c]/mat->data[r][c];
+	double temp;
+	for(int j=c-1;i>0;i--){
+	        for(int i=c;i>j;i--)
+        	temp+=x->data[i] * mat->data[i][i];
+	x->data[j]=(temp+b->data[j])/mat->data[j][j];
+	}
+
+
+
 
 				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
 
